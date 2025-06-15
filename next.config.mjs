55bf1303cd828416280396bfdda8947b5f1a33e3
Antara Chanda <antara00418@gmail.com>
2025-06-15 17:57:32 +0530

    feat: add user testimonials section with avatars

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "randomuser.me",
-        pathname: "/api/portraits/men/**",
-      },
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        pathname: "/api/portraits/women/**",
       },
     ],
   },
-   experimental: {
-    serverActions: {
-      bodySizeLimit: "5mb",
-    },
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
